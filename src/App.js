931674f54bdb745b40464a42632fc889e8ac20ae
fadestro9 <119642512+fadestro9@users.noreply.github.com>
2025-07-09 19:39:00 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do App (Frontend)</h1>
+      <p>Esta es una interfaz React para tu aplicación To Do.</p>
+    </div>
+  );
+}
+
+export default App;
